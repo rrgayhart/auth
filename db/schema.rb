d4d20e365e9f9914fa42661ffc13092ac1310f70
Meeka <rrgayhart@gmail.com>
2013-09-09 23:03:50 -0600

    Adds bcrypt and password_digest to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910042658) do
+ActiveRecord::Schema.define(version: 20130910050124) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130910042658) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
